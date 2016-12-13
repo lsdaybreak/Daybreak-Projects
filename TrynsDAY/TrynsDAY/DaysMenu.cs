f34260ff7ecf892ea5a491620512002c8253a1c5
lsdaybreak <stahlschmidtleon@web.de>
2016-12-13 18:42:48 +0100

    First Attempt

---
+++ TrynsDAY/TrynsDAY/DaysMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+using SharpDX;
+
+namespace TrynsDAY
+{
+
+    class DaysMenus
+    {
+        public static void DaysMenu()
+        {
+            Program.DaysMenu = new Menu("TrynsDay", "TrynsDay", true).SetFontStyle(FontStyle.Bold, SharpDX.Color.DarkOrange);
+            Program.DaysMenu.AddToMainMenu();
+
+            Menu DaysOrbwalker = new Menu("Orbwalker", "Orbwalker");
+            Program.orbwalker = new MyOrbwalker.Orbwalker(DaysOrbwalker);
+            Program.DaysMenu.AddSubMenu(DaysOrbwalker);
+            
+
+        }
+        
+
+    }
+}
---
+++ TrynsDAY/TrynsDAY/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+
+namespace TrynsDAY
+{
+    class Program
+    {
+        private static Spell Q, W, E, R;
+        public static MyOrbwalker.Orbwalker orbwalker;
+        public static Menu DaysMenu;
+
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.ChampionName != "Tryndamere")
+                return;
+
+            Q = new Spell(SpellSlot.Q);
+            W = new Spell(SpellSlot.W, 880f);
+            E = new Spell(SpellSlot.E, 660f);
+            R = new Spell(SpellSlot.R);
+
+
+            E.SetSkillshot(0.25f, 225f, 700f, false, SkillshotType.SkillshotLine);
+            Game.PrintChat("<font color ='#ea812e'>Hey it's DayBreak</font> <font color='#b756c5'>, have fun using TrynsDay</font>");
+
+            DaysMenus.DaysMenu();
+
+
+
+        }
+
+
+    }
+}
